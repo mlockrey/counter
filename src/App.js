8035ea3ef07c6f924b90708dbafb576ceeb1a74e
Michael Lockrey <mlockrey@192-168-1-110.tpgi.com.au>
2019-08-28 22:33:46 +1000

    Add class based react code for counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    count: 0
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
+
+  reset = () => {
+    this.setState({ count: 0 })
+  }
+
   render() {
     return (
-      <div>
-        Hello from Mike L
+      <div style={{ display: 'grid', justifyContent: 'center' }}>
+        <p style={{ fontStyle: 'sans-serif', fontSize: '50px', textAlign: 'center' }}>{this.state.count}</p>
+        <button onClick={this.increment} style={{ fontStyle: 'sans-serif', fontSize: '30px', margin: '15px', padding: '20px' }}>Increment</button>
+        <button onClick={this.decrement} style={{ fontStyle: 'sans-serif', fontSize: '30px', margin: '15px', padding: '20px' }}>Decrement</button>
+        <button onClick={this.reset} style={{ fontStyle: 'sans-serif', fontSize: '30px', margin: '15px', padding: '20px' }}>Reset</button>
       </div>
     );
   }    
